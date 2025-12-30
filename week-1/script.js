4b58aeb59d75c9c9657a5d517278572c5634cb27
zahidayturan <21060607@stu.omu.edu.tr>
2025-12-30 20:47:54 +0300

    init core layout for body and html

--- week-1/script.js
+++ week-1/script.js
@@ -1,47 +1,39 @@
-const themeIcons = document.querySelectorAll(".icon");
-const currentTheme = localStorage.getItem("theme");
-
 document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById("themeButton");
+    const themeIcons = document.querySelectorAll(".icon");
+    const currentTheme = localStorage.getItem("theme");
 
-    const btn = document.getElementById("modeToggle");
-
-    if (currentTheme === "light") {
+    // Sayfa yüklendiğinde hafızadaki temayı uygula
+    if (currentTheme === "dark") {
+        setDarkMode();
+    } else {
         setLightMode();
     }
 
-
     btn.addEventListener("click", function () {
-        btn.classList.toggle("active");
-        setTheme();
+        const isDark = document.documentElement.getAttribute("theme") === "dark";
+        if (isDark) {
+            setLightMode();
+        } else {
+            setDarkMode();
+        }
     });
 
-    function setTheme() {
-        const currentTheme = document.body.getAttribute("theme");
-
-        if (currentTheme === "light") {
-            setDarkMode();
-        } else {
-            setLightMode();
-        }
-    }
-
     function setDarkMode() {
-        document.body.removeAttribute("theme");
+        document.documentElement.setAttribute("theme", "dark");
         localStorage.setItem("theme", "dark");
 
-        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#00140F");
         themeIcons.forEach((icon) => {
-            icon.src = icon.getAttribute("src-dark");
+            icon.src = icon.getAttribute("src-light");
         });
     }
 
     function setLightMode() {
-        document.body.setAttribute("theme", "light");
+        document.documentElement.removeAttribute("theme");
         localStorage.setItem("theme", "light");
 
-        document.querySelector('meta[name="theme-color"]').setAttribute("content", "#F0F1F2");
         themeIcons.forEach((icon) => {
-            icon.src = icon.getAttribute("src-light");
+            icon.src = icon.getAttribute("src-dark");
         });
     }
 });
